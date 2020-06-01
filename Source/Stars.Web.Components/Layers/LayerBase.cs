ccbe95db9b66bd0ec8ec6b1056cfee86a24bff4b
Fredrik Flygelholm <fredrik.flygelholm@outlook.com>
2020-06-01 18:32:19 +0200

    Added LayerBase.

---
+++ Source/Stars.Web.Components/Layers/LayerBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Stars.Core;
+using Stars.Infrastructure.Data;
+
+namespace Stars.Web.Components.Layers
+{
+	public class LayerBase : ComponentBase
+	{
+		[CascadingParameter(Name = "GameWrapper")]
+		public GameWrapper GameWrapper { get; set; }
+
+		[CascadingParameter(Name = "UIWrapper")]
+		public UIWrapper UIWrapper { get; set; }
+
+		[Parameter]
+		public bool IsEnabled { get; set; } = true;
+
+		[Inject]
+		protected IJSRuntime JSRuntime { get; set; }
+
+		protected IGalaxy Galaxy => GameWrapper.Client.GalaxyView;
+
+		protected double Zoom => UIWrapper.Zoom;
+	}
+}
